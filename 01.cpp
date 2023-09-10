6d183e862094a856874f1ac6c11b836375c357ce
Andrey <andrey.zalozhenkov@pikta.ru>
2023-09-10 15:41:02 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v {4, 7, 9, 14, 12};
+
+	std::for_each(v.begin(), v.end(), [](int& elem)
+		{
+			std::cout << elem << ' ';
+		});
+	std::cout << '\n';
+	std::for_each(v.begin(), v.end(), [](int& elem)
+		{
+			if (!(elem % 2)) { elem = elem << 1; }
+		});
+
+	std::for_each(v.begin(), v.end(), [](int& elem)
+		{
+			std::cout << elem << ' ';
+		});
+	std::system("pause");
+	return 0;
+}
